d6b4691d5d4d071d3b0e2c8f652670a3b4a82d8f
andpp77 <166177111+andpp77@users.noreply.github.com>
2024-08-19 21:34:32 -0300

    commit1

---
+++ htmlCssJavascript/aula02/javas.js
@@ -0,0 +1,21 @@
+function maiordeidade(){
+	let idade = Number(prompt("Digite a sua idade:"));
+	if(idade >= 18){
+	alert("maior de idade");
+	console.log("Maior de idade");
+        }else{
+	alert("menor de idade");
+	console.log("Menor de Idade");
+        }
+}
+
+function ands(){
+	let variavel;
+	const PI = 3.14
+	let variavel2;
+	let nome = "Anderson";
+	let last_name = "Santiago";
+	alert("Boa noite "+nome);
+
+	
+}
